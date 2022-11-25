1a3324db5a6268e40d30cbad89876b526d658926
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-25 22:15:09 +0600

    test react query

--- src/Pages/Categories/Category.js
+++ src/Pages/Categories/Category.js
@@ -2,14 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import OpenProductModal from './OpenProductModal/OpenProductModal';
 import SingleCategory from './SingleCategory';
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+import Loading from '../../Loading/Loading';
 
 const Category = () => {
     const [categoryService, setCategoryService] = useState([]);
     const [bookingProduct, setBookingProduct] = useState(null)
-    const service = useLoaderData()
-    // console.log(service);
 
-    console.log(categoryService)
+
+    const service = useLoaderData()
+    // console.log(categoryService)
+
+
     useEffect(() => {
         fetch(`http://localhost:5000/allCategoryProducts?categoryName=${service?.category}`)
             .then(res => res.json())
@@ -17,6 +27,20 @@ const Category = () => {
     }, [service?.category])
 
 
+
+    //implement last time
+    // const { data: categoryService = [], isLoading } = useQuery({
+    //     queryKey: ['categoryService', service?.category],
+    //     queryFn: async () => {
+    //         const res = await fetch(`http://localhost:5000/allCategoryProducts?categoryName=${service?.category}`)
+    //         const data = await res.json();
+    //         return data;
+    //     }
+    // })
+
+    // if (isLoading) {
+    //     return <Loading></Loading>
+    // }
 
 
     return (
