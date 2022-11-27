c7faf67c286e2d6940773a8def12d0375d63d263
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-27 19:28:40 +0600

    delete buyer

--- src/Pages/DashBoard/ALlBuyers/Allbuyers.js
+++ src/Pages/DashBoard/ALlBuyers/Allbuyers.js
@@ -1,19 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import Loading from '../../../Loading/Loading';
+import ConfirmModal from '../../../Shared/ComfirmModal/ConfirmModal';
+import toast, { Toaster } from 'react-hot-toast';
+import { useQuery, } from '@tanstack/react-query'
 
 const Allbuyers = () => {
-    const [allBuyers, setAllBuyers] = useState([])
-    const [isLoading, setIsloading] = useState(true)
+    const [deletingUser, setDeletingUser] = useState(null)
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/buyers`)
+    const closeModal = () => {
+        setDeletingUser(null);
+    }
+
+
+
+
+    const { data: buyers = [], refetch, isLoading } = useQuery({
+        queryKey: [],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/buyers`);
+            const data = await res.json();
+            const allBuyersData = data.filter(buyer => (buyer.userType !== 'Seller' && buyer.role !== 'admin'));
+            console.log(allBuyersData);
+            return allBuyersData;
+        }
+    });
+
+
+    const handleDeleteUser = user => {
+        console.log(user)
+        fetch(`http://localhost:5000/users/${user._id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
             .then(res => res.json())
-            .then(buyerData => {
-                const allBuyersData = buyerData.filter(buyer => (buyer.userType !== 'Seller' && buyer.role !== 'admin'));
-                setAllBuyers(allBuyersData)
-                setIsloading(false)
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+
+                    refetch();
+                    toast.success(`Doctor ${user.name} Delete SuccessFully`)
+                }
             })
-    }, [])
+    }
+
+
 
     if (isLoading) {
         return <Loading></Loading>
@@ -36,18 +68,22 @@ const Allbuyers = () => {
                     </thead>
                     <tbody>
                         {
-                            allBuyers.map((buyer, i) => <tr key={buyer._id}>
+                            buyers.map((buyer, i) => <tr key={buyer._id}>
                                 <th>{i + 1}</th>
                                 <td>{buyer?.name}</td>
                                 <td>{buyer?.email}</td>
 
-                                <td><button className='btn btn-primary'>X</button></td>
+                                {/* The button to open modal */}
+                                <label onClick={() => { setDeletingUser(buyer) }} htmlFor="confirmation-modal" className="btn btn-danger">X</label>
 
                             </tr>)
                         }
                     </tbody>
                 </table>
             </div>
+            {
+                deletingUser && <ConfirmModal title={`Are you want to delete?`} closeModal={closeModal} handleDeleteUser={handleDeleteUser} modalDAta={deletingUser}></ConfirmModal>
+            }
         </div>
     );
 };
