af18b5c9f2ddd87487fc20594bf2bc609a49362f
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-27 20:20:47 +0600

    delete selleroption

--- src/Pages/DashBoard/Seller/AllSeller.js
+++ src/Pages/DashBoard/Seller/AllSeller.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import {
     useQuery,
 } from '@tanstack/react-query'
 import Loading from '../../../Loading/Loading';
+import ConfirmModal from '../../../Shared/ComfirmModal/ConfirmModal';
 
 const AllSeller = () => {
     // const [users, setUsers] = useState([])
     // const [sellers, setSellers] = useState([]);
+    const [deletingSeller, setDeletigSeller] = useState(null)
 
 
+    const closeModal = () => {
+        setDeletigSeller(null);
+    }
 
-    const { data: sellers = [], isLoading } = useQuery({
+
+    const { data: sellers = [], isLoading, refetch } = useQuery({
         queryKey: [],
         queryFn: async () => {
             const res = await fetch(`http://localhost:5000/sellers?userType=Seller`)
@@ -28,6 +35,25 @@ const AllSeller = () => {
     //         .then(res => res.json())
     //         .then(data => setSellers(data))
     // }, [users?.userType])
+
+    const handleDeleteUser = user => {
+        console.log(user)
+        fetch(`http://localhost:5000/users/${user._id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+
+                    refetch();
+                    toast.success(`Doctor ${user.name} Delete SuccessFully`)
+                }
+            })
+    }
 
     console.log(sellers);
 
@@ -53,19 +79,21 @@ const AllSeller = () => {
                                 <td>{seller?.name}</td>
                                 <td>{seller?.email}</td>
                                 <td>
-                                    {/* {
-                                        user?.role !== "admin" && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-xs btn-primary'>Make Admin</button>
-                                    } */}
+
                                     <button className='btn btn-primary'>verify</button>
 
                                 </td>
-                                <td><button className='btn btn-primary'>X</button></td>
+                                {/* The button to open modal */}
+                                <label onClick={() => { setDeletigSeller(seller) }} htmlFor="confirmation-modal" className="btn btn-danger btn-sm mt-2">X</label>
 
                             </tr>)
                         }
                     </tbody>
                 </table>
             </div>
+            {
+                deletingSeller && <ConfirmModal title={`Are you want to delete?`} closeModal={closeModal} handleDeleteUser={handleDeleteUser} modalDAta={deletingSeller}></ConfirmModal>
+            }
         </div>
     );
 };
