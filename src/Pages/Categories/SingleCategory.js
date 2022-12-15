5dc59fcfc8268b692da2f8b499ad01425ed89200
Tauhidul Islam Robin <robin13690@gmail.com>
2022-12-15 18:00:22 +0600

    edit product page

--- src/Pages/Categories/SingleCategory.js
+++ src/Pages/Categories/SingleCategory.js
@@ -10,31 +10,30 @@ const SingleCategory = ({ singleCategory, setBookingProduct }) => {
                 <figure className="px-10 pt-10">
                     <img src={img} alt="Shoes" className="rounded-xl" />
                 </figure>
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title">{name}</h2>
+                <div className="card-body items-left text-left font-bold">
+                    <h2 className="card-title ">{name}</h2>
                     <p>Original Price : {price}</p>
                     <p>Resell price : {newPrice}</p>
                     <p>Used Time : {usedYear} year</p>
                     <p>Location : {location}</p>
                     <p>Post Date : {postDate}</p>
-                    <div>
-                        <div className="avatar indicator my-3">
-                            <span className="indicator-item badge badge-success"></span>
-                            <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <div className='flex justify-left items-center'>
+                        <div className="avatar indicator my-3 mr-4">
+                            <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                                 <img src={sellerImg} alt='no img' />
                             </div>
                         </div>
                         <div>
                             {
-                                sellerName ? <h4>Seller : {sellerName}</h4> : <p>No Name</p>
+                                sellerName ? <h4 className='font-bold'>Seller : {sellerName}</h4> : <p>No Name</p>
                             }
                         </div>
                     </div>
-                    <div className="card-actions">
+                    <div className="card-actions flex justify-center">
                         {/* The button to open modal */}
-                        <label htmlFor="BookingProductModal" className="btn btn-primary" onClick={() => setBookingProduct(singleCategory)} >Book now</label>
+                        <label htmlFor="BookingProductModal" className="btn btn-primary" onClick={() => setBookingProduct(singleCategory)} >Buy now</label>
+                        <Link to={`/product/${_id}`} className="btn btn-active btn-link text-red-600">Report to admin</Link>
                     </div>
-                    <Link to={`/product/${_id}`} className="btn btn-active btn-link text-red-600">Report to admin</Link>
                 </div>
             </div>
         </div>
