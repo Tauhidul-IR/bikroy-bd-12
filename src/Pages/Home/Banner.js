1c5c0a39e4352ba43c25b6f38e0651f5710992e3
Tauhidul Islam Robin <robin13690@gmail.com>
2022-12-21 08:43:50 +0600

    edit banner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
                     <div className="max-w-md">
                         <h1 className="mb-5 text-5xl font-bold ">BikroyBd</h1>
                         <p className="mb-5 text-2xl">It is a most popular 2nd hand product sell's website in Our Country.</p>
-                        <button className="btn btn-primary text-white">Get Started</button>
+                        <button className="btn btn-primary text-white">Welcome</button>
                     </div>
                 </div>
             </div>
